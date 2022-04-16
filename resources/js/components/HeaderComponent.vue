c3dbe6f6981c590b5febbec6a11ce429f777b2b4
Sergen Temel <sergentemel@teknasyon.com>
2022-04-16 03:21:11 +0300

    [feat] vue imported

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <header class="d-flex justify-content-center py-3">
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+            </ul>
+        </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/js/components/MainComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <HeaderComponent></HeaderComponent>
+        <div>
+            Content Here
+        </div>
+        <FooterComponent></FooterComponent>
+    </div>
+</template>
+<script>
+import HeaderComponent from "./HeaderComponent";
+
+export default {
+    components: {
+        HeaderComponent,
+    },
+}
+</script>
+
+<style>
+main > .container {
+    padding: 60px 15px 0;
+}
+</style>
